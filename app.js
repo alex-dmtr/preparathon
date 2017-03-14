aeb61cee66e691a7342ebadff687dd5ccf7e7488
Alex Dumitru <alexadrian.dmtr@gmail.com>
2017-03-14 12:25:10 +0200

    Hello world!

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express')
+
+var app = express()
+
+var router = express.Router()
+
+router.route('/')
+    .get(function(req, res) {
+        res.status(200).send('Hello world!')
+    })
+    
+app.get('/', function(req, res) {
+    res.status(200).json({
+        api: '/api'
+    })
+})
+app.use('/api', router)
+
+module.exports = app
