efce3ec6ab6bd82132bc14183a6bf2a439bb6417
alex-dmtr <alexadrian.dmtr@gmail.com>
2017-05-02 05:22:30 +0300

    Add pem error checking

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var winston = require('winston')
 const port = process.env.PORT
 
 pem.createCertificate({days:1, selfSigned:true}, function(err, keys){
-
+  if (err) throw err;
   https.createServer({key: keys.serviceKey, cert: keys.certificate}, app)
     .listen(port, () => {
       winston.info(`https listening on ${port}`)
